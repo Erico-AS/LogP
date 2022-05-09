7708922dd11ecc105d85c691e877ecff21281450
Érico Augusto <67467324+Erico-AS@users.noreply.github.com>
2022-05-09 11:00:33 -0300

    Add files via upload

---
+++ AppCalc-20220509T134112Z-001/AppCalc/AppCalc/FormCalc.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace AppCalc
+{
+    partial class FormCalc
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCalc));
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSomar = new System.Windows.Forms.Button();
+            this.btnSubtrair = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.btnPorcentagem = new System.Windows.Forms.Button();
+            this.btnApagar = new System.Windows.Forms.Button();
+            this.lblVal1 = new System.Windows.Forms.Label();
+            this.lblVal2 = new System.Windows.Forms.Label();
+            this.txtVal2 = new System.Windows.Forms.TextBox();
+            this.txtVal1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.Yellow;
+            this.btnLimpar.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Bold);
+            this.btnLimpar.Location = new System.Drawing.Point(44, 350);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 1;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnSomar
+            // 
+            this.btnSomar.BackColor = System.Drawing.Color.Lime;
+            this.btnSomar.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSomar.Location = new System.Drawing.Point(44, 147);
+            this.btnSomar.Name = "btnSomar";
+            this.btnSomar.Size = new System.Drawing.Size(75, 23);
+            this.btnSomar.TabIndex = 2;
+            this.btnSomar.Text = "Somar";
+            this.btnSomar.UseVisualStyleBackColor = false;
+            this.btnSomar.Click += new System.EventHandler(this.btnSomar_Click);
+            // 
+            // btnSubtrair
+            // 
+            this.btnSubtrair.BackColor = System.Drawing.Color.Lime;
+            this.btnSubtrair.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Bold);
+            this.btnSubtrair.Location = new System.Drawing.Point(44, 184);
+            this.btnSubtrair.Name = "btnSubtrair";
+            this.btnSubtrair.Size = new System.Drawing.Size(75, 23);
+            this.btnSubtrair.TabIndex = 3;
+            this.btnSubtrair.Text = "Subtrair";
+            this.btnSubtrair.UseVisualStyleBackColor = false;
+            this.btnSubtrair.Click += new System.EventHandler(this.btnSubtrair_Click);
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.BackColor = System.Drawing.Color.Lime;
+            this.btnMultiplicar.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Bold);
+            this.btnMultiplicar.Location = new System.Drawing.Point(39, 269);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(85, 23);
+            this.btnMultiplicar.TabIndex = 4;
+            this.btnMultiplicar.Text = "Multiplicar";
+            this.btnMultiplicar.UseVisualStyleBackColor = false;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.BackColor = System.Drawing.Color.Lime;
+            this.btnDividir.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Bold);
+            this.btnDividir.Location = new System.Drawing.Point(44, 229);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(75, 23);
+            this.btnDividir.TabIndex = 5;
+            this.btnDividir.Text = "Dividir";
+            this.btnDividir.UseVisualStyleBackColor = false;
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
+            // 
+            // btnPorcentagem
+            // 
+            this.btnPorcentagem.BackColor = System.Drawing.Color.Lime;
+            this.btnPorcentagem.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Bold);
+            this.btnPorcentagem.Location = new System.Drawing.Point(31, 311);
+            this.btnPorcentagem.Name = "btnPorcentagem";
+            this.btnPorcentagem.Size = new System.Drawing.Size(98, 23);
+            this.btnPorcentagem.TabIndex = 6;
+            this.btnPorcentagem.Text = "Porcentagem";
+            this.btnPorcentagem.UseVisualStyleBackColor = false;
+            this.btnPorcentagem.Click += new System.EventHandler(this.btnPorcentagem_Click);
+            // 
+            // btnApagar
+            // 
+            this.btnApagar.BackColor = System.Drawing.Color.Red;
+            this.btnApagar.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Bold);
+            this.btnApagar.Location = new System.Drawing.Point(44, 394);
+            this.btnApagar.Name = "btnApagar";
+            this.btnApagar.Size = new System.Drawing.Size(75, 23);
+            this.btnApagar.TabIndex = 18;
+            this.btnApagar.Text = "Apagar";
+            this.btnApagar.UseVisualStyleBackColor = false;
+            this.btnApagar.Click += new System.EventHandler(this.btnApagar_Click);
+            // 
+            // lblVal1
+            // 
+            this.lblVal1.AutoSize = true;
+            this.lblVal1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVal1.Location = new System.Drawing.Point(27, 24);
+            this.lblVal1.Name = "lblVal1";
+            this.lblVal1.Size = new System.Drawing.Size(107, 19);
+            this.lblVal1.TabIndex = 20;
+            this.lblVal1.Text = "Primeiro valor";
+            // 
+            // lblVal2
+            // 
+            this.lblVal2.AutoSize = true;
+            this.lblVal2.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))));
+            this.lblVal2.Location = new System.Drawing.Point(292, 24);
+            this.lblVal2.Name = "lblVal2";
+            this.lblVal2.Size = new System.Drawing.Size(107, 19);
+            this.lblVal2.TabIndex = 21;
+            this.lblVal2.Text = "Segundo valor";
+            // 
+            // txtVal2
+            // 
+            this.txtVal2.Font = new System.Drawing.Font("Microsoft YaHei UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtVal2.Location = new System.Drawing.Point(296, 60);
+            this.txtVal2.Name = "txtVal2";
+            this.txtVal2.Size = new System.Drawing.Size(138, 48);
+            this.txtVal2.TabIndex = 19;
+            this.txtVal2.Text = "0";
+            this.txtVal2.Click += new System.EventHandler(this.txtVal2_Click);
+            this.txtVal2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtVal2_KeyPress);
+            // 
+            // txtVal1
+            // 
+            this.txtVal1.Font = new System.Drawing.Font("Microsoft YaHei UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtVal1.Location = new System.Drawing.Point(31, 60);
+            this.txtVal1.Name = "txtVal1";
+            this.txtVal1.Size = new System.Drawing.Size(138, 48);
+            this.txtVal1.TabIndex = 0;
+            this.txtVal1.Text = "0";
+            this.txtVal1.Click += new System.EventHandler(this.txtVal1_Click);
+            this.txtVal1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtVal1_KeyPress);
+            // 
+            // FormCalc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(522, 500);
+            this.Controls.Add(this.lblVal2);
+            this.Controls.Add(this.lblVal1);
+            this.Controls.Add(this.txtVal2);
+            this.Controls.Add(this.btnApagar);
+            this.Controls.Add(this.btnPorcentagem);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnSubtrair);
+            this.Controls.Add(this.btnSomar);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.txtVal1);
+            this.Name = "FormCalc";
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSomar;
+        private System.Windows.Forms.Button btnSubtrair;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.Button btnPorcentagem;
+        private System.Windows.Forms.Button btnApagar;
+        private System.Windows.Forms.Label lblVal1;
+        private System.Windows.Forms.Label lblVal2;
+        private System.Windows.Forms.TextBox txtVal2;
+        private System.Windows.Forms.TextBox txtVal1;
+    }
+}
+
---
+++ AppCalc-20220509T134112Z-001/AppCalc/AppCalc/FormCalc.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppCalc
+{
+    public partial class FormCalc : Form
+    {
+        public FormCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSomar_Click(object sender, EventArgs e)
+        {
+            float val1 = float.Parse(txtVal1.Text);
+            float val2 = float.Parse(txtVal2.Text);
+            float varSoma = val1 + val2; 
+             MessageBox.Show("O resultado foi " + varSoma );
+        }
+
+        private void btnSubtrair_Click(object sender, EventArgs e)
+        {
+            float val1 = float.Parse(txtVal1.Text);
+            float val2 = float.Parse(txtVal2.Text);
+            float varSub = val1 - val2;
+            MessageBox.Show("O resultado foi " + varSub);
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            float val1 = float.Parse(txtVal1.Text);
+            float val2 = float.Parse(txtVal2.Text);
+            float varMult = val1 * val2;
+            MessageBox.Show("O resultado foi " + varMult);
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            float val1 = float.Parse(txtVal1.Text);
+            float val2 = float.Parse(txtVal2.Text);
+            float varDiv = val1 / val2;
+            if (val2 == 0)
+            {
+                MessageBox.Show("Impossivel dividir por 0");
+            }
+            MessageBox.Show("O resultado foi " + varDiv);
+        }
+
+        private void btnPorcentagem_Click(object sender, EventArgs e)
+        {
+            float val1 = float.Parse(txtVal1.Text);
+            float val2 = float.Parse(txtVal2.Text);
+            float varPor = (val1 * val2) / 100;
+            MessageBox.Show("O resultado foi " + varPor);
+        }
+
+        private void btnApagar_Click(object sender, EventArgs e)
+        {
+            txtVal2.Text = "0";
+            MessageBox.Show("Você apagou o ultimo valor");
+        }
+
+        private void txtVal1_Click(object sender, EventArgs e)
+        {
+            txtVal1.Text = " ";
+        }
+
+        private void txtVal2_Click(object sender, EventArgs e)
+        {
+            txtVal2.Text = " ";
+            
+        }
+
+        private void txtVal1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtVal2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtVal1.Text = "0";
+            txtVal2.Text = "0";
+            MessageBox.Show("Você apagou todos os valores");
+        }
+    }
+}
